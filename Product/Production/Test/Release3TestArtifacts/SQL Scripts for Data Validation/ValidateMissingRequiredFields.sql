7b24b0e12cf628362120c3944dd2eb7e89b963e3
Ralph Saunders <ralph@keystone.com>
2012-06-20 16:12:39 -0400

    Moved files to a directory name that does not contain spaces.

--- Product/Production/Test/Release3TestArtifacts/SQL Scripts for Data Validation/ValidateMissingRequiredFields.sql
+++
@@ -1,4 +0,0 @@
-
-# Validate no data in inserted into the database when a file with a missing required field is processed
-select count(*) from schiepph.syndromic_surveillance where facility_name in
-('TestMissingAge','TestMissingVisitingId','TestMissingVisitDateTime','TestMissingReportDateTime','TestMissingPatientId','TestMissingFacilityId231','TestMissingFacilityId251','TestMissingDiagnosisType','TestMissingAgeUnits','TestMissingVisitType')
