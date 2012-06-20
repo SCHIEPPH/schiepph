816c5297f32c87a6873e67503006abc16e6a8d7d
unknown <padmanabhanp@padmanabhanp.atdom-hc.com>
2012-06-20 16:05:13 -0400

    Add sql files for validation

---
+++ Product/Production/Test/Release3TestArtifacts/SQLScriptsforDataValidation/ValidateMissingRequiredFields.sql
@@ -0,0 +1,4 @@
+
+# Validate no data in inserted into the database when a file with a missing required field is processed
+select count(*) from schiepph.syndromic_surveillance where facility_name in
+('TestMissingAge','TestMissingVisitingId','TestMissingVisitDateTime','TestMissingReportDateTime','TestMissingPatientId','TestMissingFacilityId231','TestMissingFacilityId251','TestMissingDiagnosisType','TestMissingAgeUnits','TestMissingVisitType')
